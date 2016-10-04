56169ee4ff21d850fc2671c3c2386936c5193efa
jake <householder.33@gmail.com>
2016-10-03 20:00:57 -0400

    Add project files.

---
+++ HansonFoods/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HansonFoods.MvcApplication" Language="C#" %>
---
+++ HansonFoods/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HansonFoods.Startup))]
+namespace HansonFoods
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
